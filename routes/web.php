ff6d03ea0b7ac4e416f521efb33be5bd0642fde9
LanAnh <anh.pham25@student.passerellesnumeriques.org>
2024-03-22 19:01:44 +0700

    Midterm2

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\BlogController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/blog', function () {
+//     return view('layouts.master');
+// })->name('blog');
+
+Route::get('/blog', [BlogController::class, 'index'] )->name('blog');
